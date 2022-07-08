1e14ecf7f7d2709f76f4b9b30344ed832bbd745c
bong <wkdgusqhd080@naver.com>
2022-07-08 12:48:53 +0900

    delete 미사용 디펜던시 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from './store/index';
+// import { Provider } from 'react-redux';
+// import { store } from './store/index';
 
 import HomePage from './pages/home';
 
 function App() {
   return (
-    <Provider store={store}>
-      <HomePage />
-    </Provider>
+    // <Provider store={store}>
+    <HomePage />
+    // </Provider>
   );
 }
 
